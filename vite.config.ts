135b19b21f76b62733caeb3e6d62719ceb414687
Mohamed Ahmed Slama <salmaabo341@gmail.com>
2025-11-16 13:05:34 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,40 +1,16 @@
-import { defineConfig, Plugin } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-let expressPlugin: Plugin | undefined;
-
-if (process.env.NODE_ENV !== "production") {
-  // Import server only in dev
-  const { createServer } = await import("./server");
-  expressPlugin = {
-    name: "express-plugin",
-    apply: "serve",
-    configureServer(server) {
-      const app = createServer();
-      server.middlewares.use(app);
-    },
-  };
-}
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    host: "::",
-    port: 8080,
-    fs: {
-      allow: ["./client", "./shared"],
-      deny: [".env", ".env.*", "*.{crt,pem}", "**/.git/**", "server/**"],
-    },
-  },
-  build: {
-    outDir: "dist/spa",
-  },
-  plugins: [react(), expressPlugin].filter(Boolean),
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./client"),
       "@shared": path.resolve(__dirname, "./shared"),
     },
+  },
+  build: {
+    outDir: "dist/spa", // الـ React build يروح هنا
   },
 });
